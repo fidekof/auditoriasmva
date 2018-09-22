dde3f9e00804eb101c6a773f6e3b7bdf6be712b2
fidekof <fidekof@gmail.com>
2018-09-22 14:45:23 -0500

    Initial commit

---
+++ src/main/java/com/mva/bodegas/auditoriasmva/AuditoriasmvaApplication.java
@@ -0,0 +1,12 @@
+package com.mva.bodegas.auditoriasmva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditoriasmvaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuditoriasmvaApplication.class, args);
+    }
+}
---
+++ src/test/java/com/mva/bodegas/auditoriasmva/AuditoriasmvaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mva.bodegas.auditoriasmva;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AuditoriasmvaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
